f8f35256c35055b1f9bf82ba562325261c04b400
Suferina <sc42695@ubt-uni.net>
2018-03-21 19:24:58 +0100

    Opsioni Select

---
+++ Econtact/econtactClasses/contactClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Econtact.ExceptionClass;
+using Econtact.InterfaceClass;
+
+namespace Econtact.econtactClasses
+{
+    public class contactClass
+    {
+        //Getter Setter Properties 
+        //Acts as Data Carrier in Our Application
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        public virtual void Validate()
+        {
+            throw new HoteliException("Not implemented (Will be impl.by child classes!");
+        }
+
+        //SElecting Data from Database
+        public DataTable Select()
+        {
+            ///Step 1: Database Connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+            try
+            {
+                //Step 2: Writing SQL Query
+                string sql = "SELECT * FROM tbl_contact";
+                //Creating cmd using sql and conn
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Creating SQL DataAdapter using cmd
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+            }
+            catch(HoteliException ex)
+            {
+                
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+
+        //Inserting DAta into Database
+        public bool Insert (contactClass c)
+        {
+            //Creating a default return type and setting its value to false
+            bool isSuccess = false;
+
+            //STep 1: Connect DAtabase
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                //STep 2: Create a SQL Query to insert DAta
+                string sql = "INSERT INTO tbl_contact (FirstName, LastName, ContactNo, Address, Gender) VALUES (@FirstName, @LastName, @ContactNo, @Address, @Gender)";
+                //Creating SQL Command using sql and conn
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Create Parameters to add data
+                cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", c.LastName);
+                cmd.Parameters.AddWithValue("@ContactNo", c.ContactNo);
+                cmd.Parameters.AddWithValue("@Address", c.Address);
+                cmd.Parameters.AddWithValue("@Gender", c.Gender);
+
+                //Connection Open Here
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                //If the query runs successfully then the value of rows will be greater than zero else its value will be 0
+                if(rows>0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (HoteliException ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
